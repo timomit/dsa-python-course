9b6eedc30d494a58d256e12a47ae511d7e8be8de
Nils Leif Fischer <hello@nilsleiffischer.de>
2018-08-19 14:46:45 +0200

    Change deployment to branch

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ cd ../../
 rm -rf release/
 nbgrader assign --create --NbGrader.config_file=source/course/nbgrader_config.py
 cd release/course/
-git commit -a -m"prepared for deploy"
-git push --force deploy master
+git branch -D deploy
+git checkout -b deploy
+git commit -a -m"Prepare for deploy"
+git push --force origin deploy
