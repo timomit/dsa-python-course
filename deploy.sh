ac11793f8bc267a16406e90aab463e1123191900
Timo Gierlich <timo@eltima-electronic.de>
2023-05-26 12:36:00 +0200

    Removed unnecessary stuff

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ../../
-rm -rf release/
-nbgrader assign --create --NbGrader.config_file=source/course/nbgrader_config.py
-cd release/course/
-git branch -D deploy
-git checkout -b deploy
-git commit -a -m"Prepare for deploy"
-git push --force origin deploy
--- nbgrader_config.py
+++
@@ -1,5 +0,0 @@
-c.CourseDirectory.assignment_id = 'course'
-c.CourseDirectory.ignore = [ 'deploy.sh' ]
-c.ClearSolutions.code_stub = {
-    'python': '# DEINE LÖSUNG HIER'
-}
